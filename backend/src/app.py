8ddcba7ca4d99a351090777aa8fa85a897a67145
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-01-31 22:19:40 -0300

    se configura el servidor y se agrega extensiones migrate,cors. Se crea archivo models que contendra las intrucciones para la creacion de la base de datos

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
-
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db
 
 app = Flask(__name__)
+app.url.map.stric_slashes = False
+app.config['DEBUG'] = True
+app.congfig['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['JWT_SECRET_KEY'] = '5c2bf15004e661d7b7c9394617143d07'
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
 
 @app.route("/")
 def index():
